97f5f578d348e281e902a4925e20d2f74218477e
StevaninRafael <100639627+StevaninRafael@users.noreply.github.com>
2022-02-28 19:53:48 -0300

    nao executa apenas abre no visual
    algumas coisas pra a uma estuda sla

---
+++ Manual Do JavaScript.bat
@@ -0,0 +1,60 @@
+Manual Do JavaScript
+{ Switch - Usado para escolha. Exemplo: 1-minutos 2-segundos se o que eu quero for os minutos
+ele vai mostrar a opcao (1) caso nao ter a opcao (1) vai executar o (2) }
+
+{ Case - Usado como opcao. Exemplo: opcao (1) opcao (2) }
+
+{ Break - Usado para parar o Switch quando o comando for executado. }
+
+{ if - Usado para determinar se uma opcao e (Verdadeira) ou (Falsa). exemplo: Se a resposta for relampago
+mas eu digitar Raio ele vai parar o Script. }
+
+{ else - Usado quando o resultado do if for Falso. Exemplo: Se voce estiver no semaforo e a cor para passar
+for verde entao voce pode passar mas se for vermelho vai ser usado o else. (PARAR) (else e usado como segunda opcao!) }
+
+{ String - Linha Do Codigo. }
+
+{ parseInt - Transformar Numeros quebrados em numeros inteiros. }
+
+{ parseFloat - Usado Para mantar um numero flutuante. }
+
+{ toFixed - Usado para aredondar um numero. Exemplo: 25.999 vai ficar 26. }
+
+{ console.log(); }
+
+{ document.write - Usado para gerar uma resposta,Usado apenas no fim de um script parar mostrar um resultado. }
+
+{ <h3> usado para escrever em um arquivo tipo htm. </h3> }
+
+{   var js_n1;               ( var - Usado como um tipo de banco de dados para definir um valor. )
+    var js_n2;                ( window.prompt - usado para usar um comando do windows para caixa de dialogo. )
+    var js_n3;                
+
+    js_n1 = parseInt(window.prompt(' informe o valor do primeiro numero: ', '00'));
+    js_n2 = parseInt(window.prompt(' informe o valor do segundo numero: ', '00')); 
+}
+
+{
+Operador | Função | Operador | Função (aqueles com (-) apenas para separar.)
++ (-) adição (-) ~ (-) complemento
+- (-) subtração (-) << (-) deslocamento à esquerda
+* (-) multiplicação (-) >> (-) deslocamento à direita
+/ (-) divisão (-) >>> (-) deslocamento à direita com zeros
+% (-) resto (-) = (-) atribuição
+++ (-) incremento (-) += (-) atribuição com adição
+-- (-) decremento (-) -= (-) atribuição com subtração
+> (-) maior que (-) *= (-) atribuição com multiplicação
+>= (-) maior ou igual (-) /= (-) atribuição com divisão
+< (-) menor que (-) %= (-) atribuição com resto
+<= (-) menor ou igual (-) &= (-) atribuição com and
+== (-) igual (-) |= (-) atribuição com or
+!= (-) não igual (-) ^= (-) atribuição com xor
+! (-) não lógico (-) <<= (-) atribuição com deslocamento à
+esquerda
+&& (-) e lógico (-) >>= (-) atribuição com deslocamento à direita
+|| (-) ou lógico (-) >>>= (-) atribuição com deslocamento à direita
+com zeros
+& (-) and (-) ?: (-) operação ternário
+^ (-) xor (-) (TIPO) (-) conversão de tipos (cast)
+| (-) or (-) INSTANCEOF (-) comparação de tipos
+}
